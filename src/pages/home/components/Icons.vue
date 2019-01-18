07de266812ef85a87f416606dc1d2c8400f2f9ff
fanfanyir <fanfanyir@163.com>
2019-01-18 23:40:05 +0800

    icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="icons">
+    <div class="icon"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+
+<style lang="stylus" scoped>
+.icons
+  overflow: hidden
+  width: 100%
+  height: 0
+  padding-bottom: 50%
+  background: green
+  .icon
+    float: left
+    width: 25%
+    padding-bottom:25%
+</style>
