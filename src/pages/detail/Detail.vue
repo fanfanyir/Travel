81de04c1fa3a02b3c1df7e3b65fbe76262f5177d
fanfanyir <fanfanyir@163.com>
2019-01-22 17:48:50 +0800

    ajax

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <detail-banner></detail-banner>
+    <detail-banner :bannerImg="bannerImg" :imgs="imgs" :name="name"></detail-banner>
     <detail-header></detail-header>
     <div class="content">
       <detail-list :list="list"></detail-list>
@@ -12,6 +12,7 @@
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
 import DetailList from './components/List'
+import axios from 'axios'
 export default {
   name: 'Detail',
   components: {
@@ -21,24 +22,36 @@ export default {
   },
   data () {
     return {
-      list: [{
-        title: '成人票',
-        children: [{
-          title: '成人三馆联票',
-          children: [{
-            title: '成人三馆联票 - 某一连锁店销售'
-          }]
-        }, {
-          title: '成人五馆联票'
-        }]
-      }, {
-        title: '学生票'
-      }, {
-        title: '儿童票'
-      }, {
-        title: '特惠票'
-      }]
+      imgs: [],
+      list: [],
+      bannerImg: '',
+      name: '',
+      id: ''
     }
+  },
+  methods: {
+    getDeatilInfo () {
+      // axios.get('/api/detail.json?id=' + this.$route.params.id)
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      }).then(this.handleGetDataSucc)
+    },
+    handleGetDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        var data = res.data
+        this.bannerImg = data.bannerImg
+        this.imgs = data.gallaryImgs
+        this.list = data.categoryList
+        this.name = data.sightName
+      }
+    }
+  },
+  mounted () {
+    this.id = this.$route.params.id
+    this.getDeatilInfo()
   }
 }
 </script>
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div class="banner" @click="handleBannerClick">
-      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1603/b5/b5437c6af5adf13290.water.jpg_600x330_2faa07c2.jpg"/>
+      <img class="banner-img" :src="bannerImg"/>
       <div class="banner-info">
         <div class="banner-title">
-          龙门古镇(AAAA景区)
+          {{name}}
         </div>
         <div class="banner-number">
           <span class="iconfont banner-icon">&#xe67f;</span>
@@ -20,16 +20,17 @@
 import CommonGallary from 'common/gallary/Gallary'
 export default {
   name: 'DetailBanner',
+  props: {
+    bannerImg: String,
+    imgs: Array,
+    name: String
+  },
   components: {
     CommonGallary
   },
   data () {
     return {
-      showGallary: false,
-      imgs: [
-        'http://img1.qunarzz.com/sight/p0/1507/5f/60b523ff677e06e47c96d9d4d3a2dc6c.water.jpg_350x240_59718973.jpg',
-        'http://img1.qunarzz.com/sight/p0/201403/07/1a01d5d962f7f523071740191b6b4be4.jpg_350x240_cd6a281d.jpg'
-      ]
+      showGallary: false
     }
   },
   methods: {
